55a3b7e493a9da7971e6487e5d58823d7fb21609
jordmantheman <jordmantheman@users.noreply.github.com>
2023-12-05 23:25:16 -0600

    feat: renders enter-your-address-form on index
    Write a component for entering an address.
    We declare some enums to use values in the frontend and backend --
    these values match what the city of madison is expecting.

--- app/routes/_index/coming-soon.tsx
+++
@@ -1,7 +0,0 @@
-import styles from "./coming-soon.module.css";
-
-export function ComingSoon() {
-  return (
-      <h1 className={styles.largeWhiteText}>Coming Soon...</h1>
-  );
-}
--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,17 +1,23 @@
 import type { MetaFunction } from "@remix-run/cloudflare";
-import { ComingSoon } from "./coming-soon";
+import { EnterYourAddressForm } from "./enter-your-address-form";
+import { Container, Paper } from '@mantine/core';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Trash and Recycling Pickup: Madison WI" },
+    { name: "description", content: "A (more) helpful schedule for browsing your trash and recycle services." },
   ];
 };
 
 export default function Index() {
   return (
-      <div>
-        <ComingSoon />
-      </div>
+    <Container miw={320} maw={1024} w="50%">
+      <Paper shadow="lg" radius="lg" withBorder p="sm">
+        <EnterYourAddressForm formProps={{
+          method: "post",
+          replace: true,
+        }} />
+      </Paper>
+    </Container>
   );
 }
