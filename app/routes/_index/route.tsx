d1e2bf1a141fac69de32eae9539a4ed699725284
jordmantheman <jordmantheman@users.noreply.github.com>
2023-12-21 15:15:09 -0600

    chore: npm run format

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,33 +1,39 @@
-import type { ActionFunctionArgs, MetaFunction } from "@remix-run/cloudflare";
-import { Container, Paper } from '@mantine/core';
-import { useActionData } from "@remix-run/react";
-import { zfd } from "zod-form-data";
-import { z } from "zod";
-import { EnterYourAddressForm } from "./enter-your-address-form";
+import type { ActionFunctionArgs, MetaFunction } from '@remix-run/cloudflare'
+import { Container, Paper } from '@mantine/core'
+import { useActionData } from '@remix-run/react'
+import { zfd } from 'zod-form-data'
+import { z } from 'zod'
+import { EnterYourAddressForm } from './enter-your-address-form'
 import { StreetDirection, StreetType } from './enums'
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "Trash and Recycling Pickup: Madison WI" },
-    { name: "description", content: "A (more) helpful schedule for browsing your trash and recycle services." },
-  ];
-};
+    { title: 'Trash and Recycling Pickup: Madison WI' },
+    {
+      name: 'description',
+      content:
+        'A (more) helpful schedule for browsing your trash and recycle services.',
+    },
+  ]
+}
 
 export default function Index() {
-  const data = useActionData<typeof action>();
+  const data = useActionData<typeof action>()
 
   return (
     <Container miw={320} maw={1024} w="50%">
       <Paper shadow="lg" radius="lg" withBorder p="sm">
-        <EnterYourAddressForm formProps={{
-          method: "post",
-          replace: true,
-        }} initialValues={data} />
+        <EnterYourAddressForm
+          formProps={{
+            method: 'post',
+            replace: true,
+          }}
+          initialValues={data}
+        />
       </Paper>
     </Container>
-  );
+  )
 }
-
 
 const schema = zfd.formData({
   streetDirection: zfd.text(z.nativeEnum(StreetDirection).optional()),
@@ -38,7 +44,8 @@ const schema = zfd.formData({
 })
 
 export const action = async ({ request }: ActionFunctionArgs) => {
-  const { streetDirection, streetName, streetNumber, streetType, unitNumber } = schema.parse(await request.formData());
+  const { streetDirection, streetName, streetNumber, streetType, unitNumber } =
+    schema.parse(await request.formData())
 
   // TODO: Write a cookie with the address, schedule, etc.
   // Need to decide what level of information will be stored on the client and
